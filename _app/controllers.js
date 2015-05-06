479e17fca89322469b5fe04ff5b6395fc3c0319e
GccOC <kylingourmand@gmail.com>
2015-05-07 01:09:57 +1000

    Upload Images

--- _app/controllers.js
+++ _app/controllers.js
@@ -9,13 +9,13 @@
 
 	m.controller('reorderController', ['$scope', '$http', function($scope, $http){
 		$scope.contacts = [
-			{ name: 'Frank', img: 'frank.jpg', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' },
-			{ name: 'Susan', img: 'susan.jpg', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' },
-			{ name: 'Emma', img: 'emma.jpg', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' },
-			{ name: 'Scott', img: 'scott.jpg', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' },
-			{ name: 'Bob', img: 'bob.jpg', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' },
-			{ name: 'Olivia', img: 'olivia.jpg', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' },
-			{ name: 'Anne', img: 'anne.jpg', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' }
+			{ name: 'Frank', imgConn: './_img/flag1.png', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' },
+			{ name: 'Susan', imgConn: './_img/flag2.png', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' },
+			{ name: 'Emma', imgConn: './_img/flag3.png', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' },
+			{ name: 'Scott', imgConn: './_img/flag4.png', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' },
+			{ name: 'Bob', imgConn: './_img/flag5.png', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' },
+			{ name: 'Olivia', imgConn: './_img/flag6.png', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' },
+			{ name: 'Anne', imgConn: './_img/flag7.png', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' }
 		];
 		$scope.onReorder = function (fromIndex, toIndex) {
 			var moved = $scope.contacts.splice(fromIndex, 1);

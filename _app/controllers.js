cb6be6f341f75197040c5ee7d4372dbe01cca5f0
GccOC <kylingourmand@gmail.com>
2015-05-07 19:04:18 +1000

    Data from Service and also update Json File

--- _app/controllers.js
+++ _app/controllers.js
@@ -7,16 +7,13 @@
 	*/
 	var m = angular.module('reorder.controllers');
 
-	m.controller('reorderController', ['$scope', '$http', function($scope, $http){
-		$scope.contacts = [
-			{ name: 'Frank', imgConn: './_img/flag1.png', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' },
-			{ name: 'Susan', imgConn: './_img/flag2.png', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' },
-			{ name: 'Emma', imgConn: './_img/flag3.png', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' },
-			{ name: 'Scott', imgConn: './_img/flag4.png', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' },
-			{ name: 'Bob', imgConn: './_img/flag5.png', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' },
-			{ name: 'Olivia', imgConn: './_img/flag6.png', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' },
-			{ name: 'Anne', imgConn: './_img/flag7.png', phone: '0101 123456', mobile: '0770 123456', email: 'frank@emailionicsorter.com' }
-		];
+	m.controller('reorderController', ['$scope', '$http', 'inforRequest', '$log', function($scope, $http, inforRequest, $log){
+		inforRequest.obtainData(retrieveDossier);
+		function retrieveDossier (dataPassedFromService) {
+			$scope.contacts = dataPassedFromService;
+			$log.log("Data bind to Ctrl."+dataPassedFromService[0].name);
+		}
+
 		$scope.onReorder = function (fromIndex, toIndex) {
 			var moved = $scope.contacts.splice(fromIndex, 1);
 			$scope.contacts.splice(toIndex, 0, moved[0]);
--- _app/services.js
+++ _app/services.js
@@ -1,1 +1,18 @@
 // JavaScript Document
+(function(){
+	var serviceM = angular.module('reorder.subject');
+
+	serviceM.service('inforRequest', ['$log', '$http', function ($log, $http) {
+		this.obtainData = function (func) {
+			$http.get('./_db/reo.json')
+			.success(function (data) {
+				func(data);
+				$log.log("Data is loaded successfully.");
+			})
+			.error(function(data, config, status) {
+				$log.log(data+" -- "+config+" -- "+status);
+			});
+		}
+	}]);
+
+})();
